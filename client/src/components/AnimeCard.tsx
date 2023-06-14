77b3e35f076434e6086058342dd164f37d59b140
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-06-14 14:37:00 -0300

    added categories list to component

--- client/src/components/AnimeCard.tsx
+++ client/src/components/AnimeCard.tsx
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Anime from "../types/anime";
+import Category from "../types/category";
+import { Link } from "react-router-dom";
 
 export default function AnimeCard(props: any) {
   const element: Anime = props.data;
 
   return (
-    <div
-      className="max-w-sm rounded flex flex-col overflow-hidden shadow-lg bg-neutral-600"
-      onClick={props.onClick}
-    >
-      <div className="flex flex-row bg-neutral-700 w-full justify-center">
-        <img
-          className="w-48"
-          src="/img_example.jpg"
-          alt="Sunset in the mountains"
-        ></img>
-      </div>
-      <div className="px-6 space-y-4">
-        <div className="pt-4">
-          <div className="font-bold text-xl">{element.name}</div>
+    <Link to={"/edit-anime/" + element.id}>
+      <li className="max-w-sm rounded flex flex-col overflow-hidden shadow-lg bg-neutral-600 cursor-pointer">
+        <div className="flex flex-row bg-neutral-700 w-full justify-center">
+          <img
+            className="w-48"
+            src="/img_example.jpg"
+            alt="Sunset in the mountains"
+          ></img>
         </div>
-        <div className="pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
-          </span>
+        <div className="px-6 space-y-4">
+          <div className="pt-4">
+            <div className="font-bold text-xl">{element.name}</div>
+          </div>
+          <ul className="pb-2">
+            {element.categories.map((c: Category) => {
+              return (
+                <li key={c.id} className="inline-block mb-2 mr-2">
+                  <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+                    {c.label}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-      </div>
-    </div>
+      </li>
+    </Link>
   );
 }
