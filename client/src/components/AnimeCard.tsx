141dcdfcf7da53ed65870b2e57dbe7beb30a6445
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-06-15 14:53:46 -0300

    deleted anime categories and elements size changed

--- client/src/components/AnimeCard.tsx
+++ client/src/components/AnimeCard.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Anime from "../types/anime";
-import Category from "../types/category";
 import { Link } from "react-router-dom";
 
 export default function AnimeCard(props: any) {
@@ -8,32 +7,18 @@ export default function AnimeCard(props: any) {
 
   return (
     <Link to={"/edit-anime/" + element.id}>
-      <li
-        className="max-w-sm rounded flex flex-col overflow-hidden shadow-lg bg-neutral-500 cursor-pointer"
-        style={{ width: "230px" }}
-      >
+      <li className="rounded flex flex-col overflow-hidden shadow-lg bg-neutral-500 cursor-pointer">
         <div className="flex flex-row bg-neutral-600 w-full justify-center">
           <img
-            className="w-48"
+            className="w-4/5"
             src="/img_example.jpg"
             alt="Sunset in the mountains"
           ></img>
         </div>
-        <div className="px-6 space-y-4">
+        <div className="px-6 pb-4 space-y-4">
           <div className="pt-4 font-bold text-xl whitespace-nowrap overflow-hidden">
             {element.name}
           </div>
-          <ul className="pb-2">
-            {element.categories.map((c: Category) => {
-              return (
-                <li key={c.id} className="inline-block mb-2 mr-2">
-                  <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-                    {c.label}
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
         </div>
       </li>
     </Link>
