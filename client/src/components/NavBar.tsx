747768b1aa10392f04ac15c51997c84ffb498fa0
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-06-22 11:15:37 -0300

    Add Anime link in NavBar deleted

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -20,32 +20,14 @@ export default function NavBar(props: any) {
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   aria-current="page"
                 >
-                  Anime List
+                  Home
                 </a>
               ) : (
                 <a
                   href="/"
                   className="block py-2 pl-3 pr-4 text-neutral-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Anime List
-                </a>
-              )}
-            </li>
-            <li>
-              {pageSelected == "add-animes" ? (
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Add Anime
-                </a>
-              ) : (
-                <a
-                  href="/add-animes"
-                  className="block py-2 pl-3 pr-4 text-neutral-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Add Anime
+                  Home
                 </a>
               )}
             </li>
