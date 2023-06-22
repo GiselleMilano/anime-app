c22970d133c8fbad125b0b856bb31ee6c760d44c
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-06-22 11:11:57 -0300

    Add anime page and comopent deleted

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,16 +1,11 @@
 import { RouteObject } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import AddAnimesPage from "./pages/AddAnimesPage";
 import EditAnimePage from "./pages/EditAnimePage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     Component: HomePage,
-  },
-  {
-    path: "/add-animes",
-    Component: AddAnimesPage,
   },
   {
     path: "/edit-anime/:id",
