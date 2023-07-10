78b9d454020e692f809a71b1e73029c6c9b97f33
ReginaEfull <32282517+GiselleMilano@users.noreply.github.com>
2023-07-10 15:47:17 -0300

    fetches the animes from the API

--- client/src/components/AnimeList.tsx
+++ client/src/components/AnimeList.tsx
@@ -6,26 +6,11 @@ export default function AnimeList() {
   const [isLoading, setIsLoading] = useState(true);
   const [animes, setAnimes] = useState<Anime[]>([]);
 
-  // Eliminar cuando este pronto el fetch a la tabla de animes.
-  const animeArray: Anime[] = [
-    {
-      id: 0,
-      name: "Naruto",
-      status: "sin comenzar",
-      description: null,
-      categories: [
-        { id: 1, label: "shonen" },
-        { id: 2, label: "comedy" },
-      ],
-    },
-  ];
-
   useEffect(() => {
-    // sustituir por fetch a la tabla de animes agregados.
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("http://localhost:3000/animes")
       .then((response) => response.json())
-      .then((dog) => {
-        setAnimes(animeArray);
+      .then((animesArray) => {
+        setAnimes(animesArray);
         setIsLoading(false);
       });
   }, []);
