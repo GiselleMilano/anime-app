5a1308a8dcefc0273e65433a0676a112db6cb535
Giselle <milanoregina8@gmail.com>
2023-08-30 14:24:49 -0300

    I added translations to each options label

--- client/src/components/SelectStatus.tsx
+++ client/src/components/SelectStatus.tsx
@@ -1,39 +1,49 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import Status from "../types/status";
 
-export default function SelectStatus(props.animeStatus: Status | null) {
-  const AnimeStatusValue : Status = props.animeStatus;
-  const [options, setOptions] = useState<Status[] | null>(null);
-  const [value, setValue] = useState<number>(0);
+interface Props {
+  currentValue: number;
+  options: Status[];
+  onChange: (value: number) => void;
+}
 
-  useEffect(() => {
-    const callback = async () => {
-      const res = await fetch("http://localhost:3000/status");
-      const json = await res.json();
-      setOptions(json);
-      if (AnimeStatusValue) {
-        setValue(AnimeStatusValue);
-      }
-    };
+export default function SelectStatus(props: Props) {
+  const options: Status[] = props.options;
 
-    callback();
-  }, []);
-
-  const onSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setValue(Number(event.target.value));
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    props.onChange(Number(event.target.value));
   };
+
+  if (options == null) {
+    return (
+      <select className="bg-neutral-600 cursor-pointer">
+        <option>Select</option>
+      </select>
+    );
+  }
 
   return (
     <select
       className="bg-neutral-600 cursor-pointer"
       id="selectStatus"
-      value={value}
-      onChange={onSelectChange}
+      value={props.currentValue}
+      onChange={onChange}
     >
       <option value={0}>Select</option>
-      {options!.map((option) => (
-        <option value={option.id}>{option.label}</option>
-      ))}
+
+      {options.map(function (option, i) {
+        let text = option.label;
+
+        if (option.label == "in_progress") {
+          text = "In Progress";
+        } else if (option.label == "listed") {
+          text = "Listed";
+        } else if (option.label == "completed") {
+          text = "Completed";
+        }
+
+        return <option value={option.id}>{text}</option>;
+      })}
     </select>
   );
 }
